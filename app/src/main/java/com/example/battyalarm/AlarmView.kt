641d0e50720ce9a439d41d9a733a9853d608de11
Tim Batty <tbatty7@gmail.com>
2022-04-01 16:31:59 -0400

    Formatted color and size of display

--- app/src/main/java/com/example/battyalarm/AlarmView.kt
+++ app/src/main/java/com/example/battyalarm/AlarmView.kt
@@ -7,9 +7,12 @@ import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.*
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.lifecycle.viewmodel.compose.viewModel
 
@@ -23,12 +26,11 @@ class AlarmView() {
                 .fillMaxSize()
                 .padding(16.dp)
         ) {
-            Text(model.title)
+            Text(text = model.title, fontSize = 40.sp, color = Color.Blue)
             Spacer(modifier = Modifier.size(20.dp))
             TextClock()
         }
     }
-
 
     @Composable
     private fun TextClock(
@@ -41,6 +43,7 @@ class AlarmView() {
             format12Hour?.let { this.format12Hour = it }
             format24Hour?.let { this.format24Hour = it }
             timeZone?.let { this.timeZone = it }
+            textSize = 70.3F
         }
         },
             modifier = modifier)
